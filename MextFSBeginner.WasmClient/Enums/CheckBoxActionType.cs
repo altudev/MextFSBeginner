15c7b879e256aad695d1b955e2009e015900865e
ALPER TUNGA <alper@altu.dev>
2024-05-14 16:38:22 +0300

    [Feature]: New Design and Algorithms added.

---
+++ MextFSBeginner.WasmClient/Enums/CheckBoxActionType.cs
@@ -0,0 +1,10 @@
+﻿namespace MextFSBeginner.WasmClient.Enums
+{
+    public enum CheckBoxActionType
+    {
+        Numbers=1,
+        Lowercase=2,
+        Uppercase = 3,
+        SpecialCharacters = 4
+    }
+}
