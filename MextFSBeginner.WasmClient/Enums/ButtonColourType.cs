c54a506e2f52b349da46c163d1f95de81e0ba4ef
ALPER TUNGA <alper@altu.dev>
2024-05-20 16:36:15 +0300

    [Feature]: MextPageTitle component has been created.

---
+++ MextFSBeginner.WasmClient/Enums/ButtonColourType.cs
@@ -0,0 +1,14 @@
+﻿namespace MextFSBeginner.WasmClient.Enums
+{
+    public enum ButtonColourType
+    {
+        Primary = 1,
+        Secondary = 2,
+        Success = 3,
+        Danger = 4,
+        Warning = 5,
+        Info = 6,
+        Light = 7,
+        Dark = 8,
+    }
+}
