6d3079ba3a8e063ca7fa7c495d98bb489519dbc3
Alper Tunga <altudev02@gmail.com>
2024-06-26 15:15:45 +0300

    [Feature] ProductController's CRUD operations completed.

--- SQLiteExamples/Controllers/ProductsController.cs
+++ SQLiteExamples/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SQLiteExamples.Data;
 using SQLiteExamples.Entities;
 using SQLiteExamples.Services;
@@ -22,6 +23,63 @@ public class ProductsController : ControllerBase
         _dbContext.Database.EnsureCreated();
     }
     
+    [HttpPut("{id}")]
+    public IActionResult Update(string id, Product product)
+    {
+        var productToUpdate = _dbContext
+            .Products
+            .FirstOrDefault(x => x.Id == id);
+        
+        if (productToUpdate is null)
+            return NotFound();
+
+        productToUpdate.Colour = product.Colour;
+        productToUpdate.Name = product.Name;
+        productToUpdate.Price = product.Price;
+        productToUpdate.Size = product.Size;
+        
+        _dbContext.SaveChanges();
+        
+        _requestCounterService.Increment();
+        
+        return Ok("Urun basariyla guncellendi.");
+    }
+    
+    [HttpGet("{id}")]
+    public IActionResult Get(string id)
+    {
+        var product = _dbContext
+            .Products
+            .AsNoTracking()
+            .FirstOrDefault(x => x.Id == id);
+        
+        if (product is null)
+            return NotFound();
+        
+        _requestCounterService.Increment();
+        
+        return Ok(product);
+    }
+    
+    [HttpDelete("{id}")]
+    public IActionResult Delete(string id)
+    {
+        var product = _dbContext
+            .Products
+            .FirstOrDefault(x => x.Id == id);
+        
+        if (product is null)
+            return NotFound();
+        
+        _dbContext.Products.Remove(product);
+        
+        _dbContext.SaveChanges();
+        
+        _requestCounterService.Increment();
+        
+        return Ok("Urun basariyla silindi.");
+    }
+    
     
     [HttpPost]
     public IActionResult Create(Product product)
@@ -42,6 +100,7 @@ public class ProductsController : ControllerBase
         var url = _myUrlService.Url;
         var products = _dbContext
             .Products
+            .AsNoTracking()
             .Where(x=>x.Price>=100)
             .ToList();
         
